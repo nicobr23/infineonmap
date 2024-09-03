35db89a7fc50af0350d6f65753512cd0b67e8eb8
SeanEsper <sean.esper@infineon.com>
2024-09-03 21:57:43 +0200

    Sean Changes V2

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
           </div>
 
           <div className="flex flex-col items-center justify-center w-full">
-            <h1 className="text-base mobile:text-xl xl:text-2xl font-bold text-ocean-80 mb-0 mobile:mb-2">Map of all favorite places</h1>
+            <h1 className="text-center text-base mobile:text-xl xl:text-2xl font-bold text-ocean-80 mb-0 px-2 mobile:mb-2">Map of all favorite places</h1>
           
             <div className="relative hidden items-center mobile:flex">
               {/* Search Bar */}
@@ -102,13 +102,12 @@ function App() {
         </div>
         <div className="flex items-center justify-center w-3-5 mobile:hidden border-b-2 border-ocean-80 pb-4">
           <div className="relative flex items-center">
-            {/* Search Bar in mobile perspective
-            */}
+            {/* Search Bar in mobile perspective*/}
             <FaSearch className="bg-ocean-20 absolute left-3 text-gray-500" />
             <input
               type="text"
               placeholder="Search..."
-              className="bg-ocean-20 pl-10 pr-4 py-2 rounded-full border border-ocean-80 focus:outline-none focus:ring-2 focus:ring-[#0A8276] text-gray-700 w-auto shadow-sm  "
+              className="bg-ocean-20 pl-10 pr-4 py-1 rounded-full border border-ocean-80 focus:outline-none focus:ring-2 focus:ring-[#0A8276] text-gray-700 w-64 shadow-sm  "
               value={searchText}
               onChange={handleSearchChange}
             />
