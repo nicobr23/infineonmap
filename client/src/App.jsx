6a8290ec09bde9d3b9340045ea1e8fdef6b43cb1
SeanEsper <sean.esper@infineon.com>
2024-09-12 18:44:33 +0200

    Sean Changes V4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -77,7 +77,46 @@ function App() {
     <div className="flex flex-col h-screen bg-white">
       {/* Header with search bar */}
       <header>
-        {/* Your header code */}
+        <div className="flex items-center justify-between p-4 bg-white border-b-0 mobile:border-b-2 border-ocean-80">
+          <div className="flex items-center w-1/5">
+            <img src={InfineonLogo} alt="Infineon Logo" className="h-6 mobile:h-12" /> {/* Infineon Logo */}
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-full">
+            <h1 className="text-center text-base mobile:text-xl xl:text-2xl font-bold text-ocean-80 mb-0 px-2 mobile:mb-2">Karte aller Lieblingsplätze</h1>
+          
+            <div className="relative hidden items-center mobile:flex">
+              {/* Search Bar */}
+              <FaSearch className="absolute left-3 text-gray-500 bg-ocean-20" />
+              <input
+                type="text"
+                placeholder="Search..."
+                className=" bg-ocean-20 pl-10 pr-4 py-2 rounded-full border border-ocean-80 focus:outline-none focus:ring-2 focus:ring-[#0A8276] text-gray-700 w-auto shadow-sm  "
+                value={searchText}
+                onChange={handleSearchChange}
+              />
+            </div>
+          </div>
+
+          <div className="w-1/5">
+            <p className="text-center"><button onClick={() => setShowModal(true)} className='px-2 py-0.5 border-ocean-80 bg-white rounded-lg max-w-max border-2 text-base mobile:text-xl xl:text-3xl font-bold text-ocean-80'>
+            Info
+            </button></p>
+          </div> {/* Placeholder to align with logo */}
+        </div>
+        <div className="flex items-center justify-center w-3-5 mobile:hidden border-b-2 border-ocean-80 pb-4">
+          <div className="relative flex items-center">
+            {/* Search Bar in mobile perspective*/}
+            <FaSearch className="bg-ocean-20 absolute left-3 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-ocean-20 pl-10 pr-4 py-1 rounded-full border border-ocean-80 focus:outline-none focus:ring-2 focus:ring-[#0A8276] text-gray-700 w-64 shadow-sm  "
+              value={searchText}
+              onChange={handleSearchChange}
+            />
+          </div>
+        </div>
       </header>
 
       {showModal && <Modal onClose={() => setShowModal(false)} />}
@@ -114,7 +153,7 @@ function App() {
                     <img
                       src={imagePaths[selectedLocation.id] || imagePaths[1]} // Fallback to '1.jpg' if no matching image
                       alt={`Image for location ${selectedLocation.id}`}
-                      className="h-16 mobile:h-32 xl:h-48"
+                      className="h-32 xl:h-48"
                     />
                     <p className="text-sm text-gray-700 pb-4">
                       Empfohlen von: {highlightText(selectedLocation.user, searchText)}
